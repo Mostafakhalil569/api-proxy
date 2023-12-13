af4770010f9f1a5acdd2466dc7ba1b908140b216
Mostafa Khalil <102040483+Mostafakhalil21@users.noreply.github.com>
2023-12-13 13:05:08 +0200

    update serverless

--- app.js
+++ app.js
@@ -5,19 +5,20 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 // Enable CORS for both local development and deployed S3 application
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
 
+const corsOptions = {
+  origin: 'https://your-angular-app.render.com',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
 
+app.use(cors(corsOptions));
 const apiEndpoint = 'https://api.xiteit.co';
 const apiProxy = createProxyMiddleware('/api', { target: apiEndpoint, changeOrigin: true });
 app.use('/api', apiProxy);
 
-const port = 3000;
+const port = process.env.RAILWAY_PORT || 3000;
 app.listen(port, () => {
   console.log(`Express server is running on http://localhost:${port}`);
 });
